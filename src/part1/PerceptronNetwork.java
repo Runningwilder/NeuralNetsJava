2cb8092244d92ed1b9d1899ce15a2a81414dd300
Runningwilder <languageblog96@gmail.com>
2020-08-22 22:53:03 +0300

    part4

--- src/part1/PerceptronNetwork.java
+++ src/part1/PerceptronNetwork.java
@@ -44,5 +44,4 @@ public class PerceptronNetwork {
 		}
 		return outputs;
 	}
-
 }
--- src/part2/SigmoidNetwork.java
+++ src/part2/SigmoidNetwork.java
@@ -6,25 +6,27 @@ public class SigmoidNetwork {
 	private final double WEIGHTS = 2.3;
 	private int numLayers;
 	private int[] sizes;
-	
+
 	public SigmoidNetwork(int... sizes) {
 		this.sizes = sizes;
 		this.numLayers = sizes.length;
 	}
-	
+
 	public static void main(String[] args) {
 		SigmoidNetwork net = new SigmoidNetwork(2, 3, 2);
-		double[] inputs = {1, 1};
+		double[] inputs = { 1, 1 };
 		double[] outputs = net.feedForward(inputs);
-		
+
 		for (int i = 0; i < outputs.length; i++) { // Prints output array consisting of float numbers between 0 and 1
 			System.out.println(outputs[i]);
 		}
 	}
+
 	/**
 	 * 
 	 * @param inputs - int[] of inputs - 1st layer also called the input layer
-	 * @return int[] containing output from the network consisting of float numbers between 0 and 1
+	 * @return int[] containing output from the network consisting of float numbers
+	 *         between 0 and 1
 	 */
 	private double[] feedForward(double[] inputs) {
 		double[] outputs = null;
@@ -32,25 +34,15 @@ public class SigmoidNetwork {
 			int size = sizes[i];
 			double[] z = new double[size];
 			outputs = new double[size];
-			for(int j = 0; j < size; j++) { // Iterates over all neurons of layer i
+			for (int j = 0; j < size; j++) { // Iterates over all neurons of layer i
 				for (int k = 0; k < inputs.length; k++) { // Iterates over all inputs for layer i
 					z[j] += WEIGHTS * inputs[k];
 				}
 				z[j] += BIAS;
-				outputs[j] =  1 / (1 + Math.exp(-z[j]));
+				outputs[j] = 1 / (1 + Math.exp(-z[j]));
 			}
 			inputs = outputs;
 		}
 		return outputs;
 	}
-
 }
-
-
-
-
-
-
-
-
-
